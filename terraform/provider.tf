fc44a21714b42b6cb8428cb1cbc6aca28b71d939
lu-tar <l.tar@protonmail.com>
2025-05-01 21:07:45 +0200

    Working deploy

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -1,23 +1,21 @@
-terraform {
-  required_version = ">= 0.13.0"
-
-  required_providers {
-    proxmox = {
-      # LINK https://github.com/Telmate/terraform-provider-proxmox
-      source = "telmate/proxmox"
-      version = "3.0.1-rc8"
-    }
-  }
-}
-
 # Check credentials.auto.tfvars
 # https://developer.hashicorp.com/terraform/language/values/variables
+# https://github.com/Telmate/terraform-provider-proxmox
 # To set lots of variables, it is more convenient to specify their values in a variable definitions file 
 # (with a filename ending in either .tfvars or .tfvars.json) and then specify that file on the command line with -var-file
 # Terraform also automatically loads a number of variable definitions files if they are present:
 #    Files named exactly terraform.tfvars or terraform.tfvars.json.
 #    Any files with names ending in .auto.tfvars or .auto.tfvars.json.
+terraform {
+  required_version = ">= 0.13.0"
 
+  required_providers {
+    proxmox = {
+      source = "telmate/proxmox"
+      version = "3.0.1-rc8"
+    }
+  }
+}
 
 variable "PROXMOX_URL" {
   type = string
@@ -38,11 +36,14 @@ variable "PUBLIC_SSH_KEY" {
   sensitive = true
 }
 
+variable "CI_PASSWORD" {
+  type      = string
+  sensitive = true
+}
+
 provider "proxmox" {
   pm_api_url = var.PROXMOX_URL
   pm_api_token_id = var.PROXMOX_USER
   pm_api_token_secret = var.PROXMOX_TOKEN
-  
-  # NOTE Optional, but recommended to set to true if you are using self-signed certificates.
-  pm_tls_insecure = false
+  pm_tls_insecure = true
 }
--- terraform/vm-deploy-1.tf
+++ terraform/vm-deploy-1.tf
@@ -1,58 +1,85 @@
-#Reference: https://github.com/ChristianLempa/boilerplates/blob/main/terraform/proxmox/vm_qemu.tf
-resource "proxmox_vm_qemu" "your-vm" {
-  name = "debian12-template"
-  desc = "description"
-  agent = 1  # <-- (Optional) Enable QEMU Guest Agent
-  target_node = "pve"
-  vmid = "100"
-  clone = "debian-tf"
-  full_clone = true
-  onboot = true
-  startup = ""
-  automatic_reboot = false
+resource "proxmox_vm_qemu" "vm-deploy-1" {
+    name = "tf-debian12-cloud"
+    # Notes inside PVE GUI
+    desc = "A test for using terraform and cloudinit"
 
-  qemu_os = "other"
-  bios = "seabios"
-  cores = 2
-  sockets = 1
-  cpu_type = "host"
-  memory = 2048
-  balloon = 1024
+    # Node name has to be the same name as within the cluster
+    # this might not include the FQDN
+    target_node = "pve"
+    vmid = 106
 
-  network {
-    id     = 0  # NOTE Required since 3.x.x
-    bridge = "vmbr2"
-    model  = "virtio"
-  }
-    
-  scsihw = "virtio-scsi-single"
+    # The destination resource pool for the new VM
+    #pool = "pool0"
 
-  disks {
-    ide {
-      ide0 {
-        cloudinit {
-          storage = "local-lvm"
-        }
-      }
+    # The template name to clone this vm from
+    clone = "debian12-template"
+    full_clone = true
+
+    # Activate QEMU agent for this VM
+    agent = 1
+
+    qemu_os = "other"
+    bios = "seabios"
+    cores = 2
+    sockets = 1
+    cpu_type = "host"
+    memory = 2048
+    balloon = 1024
+
+    automatic_reboot = false
+    startup = ""
+    onboot = true
+    vm_state = "running"
+
+    scsihw = "virtio-scsi-single"
+
+    serial {
+        id   = 0
+        type = "socket"
     }
-    virtio {
-      virtio0 {
-        disk {
-          storage = "local-lvm"
 
-          # NOTE Since 3.x.x size change disk size will trigger a disk resize
-          size = "20G"
-
-          iothread = true
-          replicate = false
+    # Setup the disk
+    disks {
+        ide {
+            ide3 {
+                cloudinit {
+                    storage = "local-lvm"
+                }
+            }
         }
-      }
+        # Check if virtio is better, exluding stats due to template cloning
+        scsi {
+            scsi0 {
+                disk {
+                    size            = 20
+                    cache           = "writeback"
+                    storage         = "local-lvm"
+                    #storage_type    = "rbd"
+                    #iothread        = true
+                    #discard         = true
+                }
+            }
+        }
     }
-  }
 
-  ipconfig0 = "ip=10.0.1.6/0,gw=10.0.1.254"
-  nameserver = "10.0.6.1"
-  ciuser = "your-username"
-  sshkeys = var.PUBLIC_SSH_KEY
+    # Setup the network interface and assign a vlan tag: 256
+    network {
+        id = 0
+        model = "virtio"
+        firewall = false
+        bridge = "vmbr2"
+        #tag = 256
+    }
 
+    # Setup the ip address using cloud-init.
+    boot = "order=scsi0"
+    ipconfig0 = "ip=10.0.1.6/24,gw=10.0.1.254"
+    nameserver = "10.0.6.1"
+    ciuser = "luca"
+    sshkeys = var.PUBLIC_SSH_KEY
+    cipassword = var.CI_PASSWORD
+
+    #sshkeys = <<EOF
+    #ssh-rsa 9182739187293817293817293871== user@pc
+    #EOF
 }
